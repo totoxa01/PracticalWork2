7c3f976cdd2f66b47e9c92c6a3d67a5217065d1f
Светлана <Totik0491@yandex.ru>
2021-04-06 14:11:36 +0300

    Задание 3

---
+++ PracticalWork2/Classes/ClassEvenNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticalWork2.Classes
+{
+    class ClassEvenNumbers
+    {
+        public static void EvenNumber()
+        {
+            Console.WriteLine("Введите число");
+            int number = Int32.Parse(Console.ReadLine());
+            if(number%2==0)
+            {
+                Console.WriteLine($" Число {number} - четное");
+            }
+            else 
+            {
+                Console.WriteLine($" Число {number} - не четное");
+            }
+        }
+    }
+}
--- PracticalWork2/Program.cs
+++ PracticalWork2/Program.cs
@@ -6,8 +6,9 @@ namespace PracticalWork2
     {        
         static void Main(string[] args)
         {
-            Console.WriteLine($"Среднесуточная температура: {ClassAverageDailyTemperature.AverageDailyTemperature()}");
-            Classes.ClassMonths.NameMonth();
+            //Console.WriteLine($"Среднесуточная температура: {ClassAverageDailyTemperature.AverageDailyTemperature()}");
+            //Classes.ClassMonths.NameMonth();
+            Classes.ClassEvenNumbers.EvenNumber();
         }
     }
 }
